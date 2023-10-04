00caa389955b4a9044522daf85653f6a96c74b4a
Aditya Lande <104717283+74604Aditya@users.noreply.github.com>
2023-10-04 22:19:08 +0530

    Add files via upload

---
+++ Day3/Operator.java
@@ -0,0 +1,15 @@
+class Operator{
+
+	public static void main( String args[] ){
+
+		int x = 5;
+		int y = 15;
+
+		if( x > 5 & y++ <20){
+
+			x++;
+		}
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
---
+++ Day3/Operator5.java
@@ -0,0 +1,16 @@
+class Operator5{
+
+	public static void main( String args[] ){
+
+		int x = 5;
+		int y = 15;
+
+		if( x >= 5 && y++ <20){
+
+			x++;
+			y = y + 5;
+		}
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
